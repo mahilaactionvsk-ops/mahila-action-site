8c0a88142b5c6f87623feca5f103ba7eb76a0a2a
Mahila Action <mahilaaction.vsk@gmail.com>
2026-07-23 12:37:28 +0530

    chore: clean up unwanted root files and update folder structure

---
+++ mahila-site-strapi/src/api/volunteer-registration/services/volunteer-registration.ts
@@ -0,0 +1,7 @@
+/**
+ * volunteer-registration service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::volunteer-registration.volunteer-registration');
